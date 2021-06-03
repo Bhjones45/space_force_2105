37816ca0781f8173e2bb61a47b2dc8d53c21bd69
Brett Jones <bhjones4545@gmail.com>
2021-06-03 09:30:22 -0600

    pass test

--- lib/spacecraft.rb
+++ lib/spacecraft.rb
@@ -1,8 +1,15 @@
 class Spacecraft
   attr_reader :name,
-              :fuel
+              :fuel,
+              :requirements
+              
   def initialize(details)
     @name = details[:name]
     @fuel = details[:fuel]
+    @requirements = []
+  end
+
+  def add_requirement(requirement)
+    @requirements << requirement
   end
 end
