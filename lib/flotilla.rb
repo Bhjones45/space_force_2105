e91d4c72d7398fbaad6a8f74cede44d6eb1fc863
Brett Jones <bhjones4545@gmail.com>
2021-06-03 09:41:44 -0600

    Pass tests

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -1,7 +1,11 @@
 class Flotilla
-  attr_reader :designation
+  attr_reader :designation,
+              :personnel,
+              :ships
 
   def initialize(goal)
     @designation = goal[:designation]
+    @personnel = []
+    @ships = []
   end
 end
