298ceb32fd836d136fb3a6e4ca5902935be36bae
Brett Jones <bhjones4545@gmail.com>
2021-06-04 10:51:02 -0600

    Complete iteration 4

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -1,10 +1,10 @@
 class Flotilla
-  attr_reader :designation,
+  attr_reader :designation, # use name
               :personnel,
               :ships
 
   def initialize(goal)
-    @designation = goal[:designation]
+    @designation = goal[:designation] #use name
     @personnel = []
     @ships = []
   end
@@ -18,13 +18,58 @@ class Flotilla
   end
 
 #messy but it worked. work on refactor later
+  # def recommend_personnel(ship)
+  #   ship.requirements.flat_map do |requirement|
+  #     @personnel.find_all do |person|
+  #       requirement.any? do |specialties, experience|
+  #         person.specialties.include?(specialties) && (person.experience >= experience)
+  #       end
+  #     end
+  #   end.uniq
+  # end
+
   def recommend_personnel(ship)
-    ship.requirements.flat_map do |requirement|
-      @personnel.find_all do |person|
-        requirement.any? do |specialties, experience|
-          person.specialties.include?(specialties) && (person.experience >= experience)
+    people = []
+
+    @personnel.each do |person|
+      ship.requirements.each do |req| #[{spec: years}]
+         person.specialties.any? do |spec| #[:spec]
+          people << person if (req.key? spec) && (person.experience >= req[spec])
+          end
         end
       end
-    end.uniq
+    people.uniq
   end
-end
+
+  def personnel_by_ship
+    #goal: build a hash, ship obj is key, value is recommend_personnel(ship_obj)
+
+    # loop over ready_ships
+      #use ship obj to call recommend_personnel
+      #ship obj will be my has key
+
+      staff_allocation = Hash.new
+
+      @ships.each do |ship|
+        staff_allocation[ship] = recommend_personnel(ship)
+      end
+      staff_allocation
+    end
+
+    def ready_ships(fuel_level)
+      ships = []
+      staff = personnel_by_ship
+      @ships.each do |ship|
+        staff[ship].each do |person|
+          ship.requirements.each do |req|
+            req.all? do |requirement, experience|
+              if person.specialties.include? requirement
+                ships << ship
+              end
+            end
+          end
+        end
+      end
+      ships.uniq
+    end
+  end
--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -92,6 +92,12 @@ RSpec.describe Spacecraft do
     end
 
     it 'can display personnel by ship' do
+      @daedalus.add_requirement({astrophysics: 6})
+      @daedalus.add_requirement({quantum_mechanics: 3})
+
+      @odyssey.add_requirement({operations: 6})
+      @odyssey.add_requirement({maintenance: 3})
+
       @seventh_flotilla.add_personnel(@kathy)
       @seventh_flotilla.add_personnel(@polly)
       @seventh_flotilla.add_personnel(@rover)
@@ -102,8 +108,43 @@ RSpec.describe Spacecraft do
       @seventh_flotilla.add_ship(@daedalus)
       @seventh_flotilla.add_ship(@odyssey)
 
-      expect(@seventh_flotilla.ships).to eq([@daedalus, @odyssey])
-      expect(@seventh_flotilla.personnel_by_ship).to eq({})
+      expected = {
+        @daedalus => [@kathy, @sampson],
+        @odyssey => [@polly]
+      }
+
+
+      expect(@seventh_flotilla.personnel_by_ship).to eq expected
+    end
+
+    it 'can ready the ships' do
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+      daedalus.add_requirement({astrophysics: 6})
+      daedalus.add_requirement({quantum_mechanics: 3})
+      prometheus = Spacecraft.new({name: 'Prometheus', fuel: 300})
+      prometheus.add_requirement({operations: 6})
+      prometheus.add_requirement({science: 3})
+      seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+      kathy = Person.new('Kathy Chan', 10)
+      kathy.add_specialty(:astrophysics)
+      kathy.add_specialty(:quantum_mechanics)
+      polly = Person.new('Polly Parker', 4)
+      polly.add_specialty(:operations)
+      polly.add_specialty(:maintenance)
+      rover = Person.new('Rover Henriette', 1)
+      rover.add_specialty(:operations)
+      rover.add_specialty(:maintenance)
+      sampson = Person.new('Sampson Edwards', 7)
+      sampson.add_specialty(:astrophysics)
+      sampson.add_specialty(:quantum_mechanics)
+      seventh_flotilla.add_personnel(kathy)
+      seventh_flotilla.add_personnel(polly)
+      seventh_flotilla.add_personnel(rover)
+      seventh_flotilla.add_personnel(sampson)
+      seventh_flotilla.add_ship(daedalus)
+      seventh_flotilla.add_ship(prometheus)
+
+      expect(seventh_flotilla.ready_ships(100)).to eq [daedalus]
     end
   end
 end
